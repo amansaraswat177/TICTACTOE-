adf40a2cf01c7890a8702de694bbdcb8bf0f5e77
amansaraswat177 <amansarswat786@gmail.com>
2024-02-04 22:32:02 +0530

    Splash Screen Added

---
+++ app/src/main/java/com/example/tictactoe/splashScreen.java
@@ -0,0 +1,29 @@
+package com.example.tictactoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class splashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent ihome;
+                ihome = new Intent(splashScreen.this,MainActivity.class);
+                startActivity(ihome);
+            }
+        },3000);
+
+
+
+
+    }
+}
